e00ad67f7952ef89d248e566f3268e044bb8b038
John Doe <johndoe@example.com>
2021-05-03 01:04:09 +0530

    Changes for Bookings page

--- src/app/booking-history/booking-history.page.ts
+++ src/app/booking-history/booking-history.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-booking-history',
   templateUrl: 'booking-history.page.html',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class BookingHistoryPage {
 
-  constructor() {}
+  constructor(public actionSheetController: ActionSheetController) {}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Charging Records',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Last Charged'
+      }, {
+        text: 'Last 7 Days'
+      }, {
+        text: 'Monthly'
+      }, {
+        text: 'Annually'
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 
 }
