bc0176843728ba5080ae766d835cb5f4f63001c5
Godson <godsonherpatt@gmail.com>
2022-03-21 14:47:48 +0530

    charge list

--- src/app/socket/socket.page.ts
+++ src/app/socket/socket.page.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocketPage implements OnInit {
   history = true;
-
+  monthOptions = [];
+  chargerOptions = [];
+  selectedMonthFilter: string;
+  selectedCharger: string;
   constructor() { }
 
   ngOnInit() {
+    const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    for (let month = 0; month <= monthNames.length - 1; month++) {
+      this.monthOptions.push({text: monthNames[month], value: monthNames[month]});
+    }
+    this.selectedMonthFilter = this.monthOptions[new Date().getMonth()].value;
   }
 
   segmentChanged() {
