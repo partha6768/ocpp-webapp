54252398ae838195ae2aa2fd205f4a349c62f289
Godson <godsonherpatt@gmail.com>
2022-03-24 23:01:03 +0530

    progress bar

--- src/app/charging/charging.module.ts
+++ src/app/charging/charging.module.ts
@@ -9,14 +9,28 @@ import {InProgressComponent} from "./in-progress/in-progress.component";
 import {StopConfirmationComponent} from "./stop-confirmation/stop-confirmation.component";
 import {ChargeCompleteComponent} from "./charge-complete/charge-complete.component";
 import {PaymentCompleteComponent} from "./payment-complete/payment-complete.component";
+import {NgCircleProgressModule} from "ng-circle-progress";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ChargingPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ChargingPageRoutingModule,
+        NgCircleProgressModule,
+        NgCircleProgressModule.forRoot({
+            "backgroundPadding": 20,
+            "radius": 200,
+            "toFixed": 0,
+            "outerStrokeWidth": 10,
+            "showSubtitle": false,
+            "showBackground": false,
+            "showInnerStroke": false,
+            "showTitle": false,
+            "showUnits": false,
+            "responsive": false,
+            "startFromZero": false})
+    ],
   declarations: [LateComponent, ChargingPage, InProgressComponent, StopConfirmationComponent, ChargeCompleteComponent, PaymentCompleteComponent]
 })
 export class ChargingPageModule {}
